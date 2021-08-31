de5efc77aa36229eefc8f23f9c3f03275e7ae7c1
oscarfalero <oscarfalero10@gmail.com>
2021-08-30 22:43:57 -0300

    Top header
    Solución de iconos y responsividad de iconos y texto

--- src/components/topheader/TopHeader.js
+++ src/components/topheader/TopHeader.js
@@ -1,27 +1,35 @@
 import React from "react";
 import "./TopHeader.css";
-import fbLogo from "./icons/fb.png";
-import inLogo from "./icons/in.png";
+import fbLogo from "./icons/fb.svg";
+import instLogo from "./icons/inst.svg";
+import mailLogo from "./icons/mail.svg";
+import waLogo from "./icons/wa.svg";
 
 // Contact Info
 let fbLink = "https://www.facebook.com/dulcecocina.uy";
 let instaLink = "https://www.instagram.com/dulcecocina.uy";
 let emailLink = "lucymarisel@gmail.com";
-let phoneLink = "[UY +598] 094.050.650";
+let phoneLink = "[UY +598]094.050.650";
 
 const TopHeader = () => {
   return (
-    <div class="container">
-      <div class="row">
-        <div class="icon_area">
+    <div className="container">
+      <div className="row">
+        <div className="icon_area">
           <a href={fbLink} target="_blank" rel="noreferrer noopener">
             <img src={fbLogo} alt="facebook" />
           </a>
           <a href={instaLink} target="_blank" rel="noreferrer noopener">
-            <img src={inLogo} alt="instagram" />
+            <img src={instLogo} alt="instagram" />
+          </a>
+          <a className="mailLogo" href={emailLink} target="_blank" rel="noreferrer noopener">
+            <img src={mailLogo} alt="email" />
+          </a>
+          <a className="wpLogo" href={phoneLink} target="_blank" rel="noreferrer noopener">
+            <img src={waLogo} alt="whatsapp" />
           </a>
         </div>
-        <div class="log_area">
+        <div className="log_area">
           <a href={emailLink}>{emailLink}</a>
           <a href={phoneLink}>{phoneLink}</a>
         </div>
