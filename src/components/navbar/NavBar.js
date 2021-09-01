d03ad661aefa1d47818daa9c96fddd0c0e092ad2
maurob94 <mauro.bassini.94@gmail.com>
2021-09-01 01:19:34 -0300

    Mobile Navbar
    -Working on mobile navbar

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,12 +1,28 @@
 import React from "react";
 import styles from "./NavBar.module.css";
-import 'boxicons';
+import homeLogo from "./icons/bx-home-alt.svg";
+import productsLogo from "./icons/bxs-offer.svg";
+import receiptsLogo from "./icons/bx-note.svg";
+import contactLogo from "./icons/bxs-contact.svg";
 
 const Navbar = () => {
   return (
     <nav className={styles.nav}>
-      <a href="#">
-      <i class='bx bxs-dashboard' ></i>
+      <a href="#" className={styles.nav_link}>
+        <img src={homeLogo} alt="Inicio" />
+        <span>Inicio</span>
+      </a>
+      <a href="#" className={styles.nav_link}>
+        <img src={productsLogo} alt="Inicio" />
+        <span>Productos</span>
+      </a>
+      <a href="#" className={styles.nav_link}>
+        <img src={receiptsLogo} alt="Inicio" />
+        <span>Recetas</span>
+      </a>
+      <a href="#" className={styles.nav_link}>
+        <img src={contactLogo} alt="Inicio" />
+        <span>Contacto</span>
       </a>
     </nav>
   );
