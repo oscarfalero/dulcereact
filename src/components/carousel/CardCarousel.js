7bd409b8964877192a9ea7ed62eee68297a090dc
oscarfalero <oscarfalero10@gmail.com>
2021-09-03 01:16:03 -0300

    Carousel

--- src/components/carousel/CardCarousel.js
+++ src/components/carousel/CardCarousel.js
@@ -1,7 +1,7 @@
 import React from "react";
-import "./Carousel.module.css";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import styles from "./CardCarousel.module.css";
 
 import img0 from "./img/0.jpg";
 import img1 from "./img/1.jpg";
@@ -37,22 +37,22 @@ const responsive = {
 const CardCarousel = () => {
   return (
     <Carousel
+      responsive={responsive}
       swipeable={true}
       draggable={false}
       showDots={true}
-      responsive={responsive}
-      ssr={true} // means to render carousel on server-side.
+      dotListClass={styles.dot}
+      // renderDotsOutside={true}
       infinite={true}
-      // autoPlay={this.props.deviceType !== "mobile" ? true : false}
-      autoPlaySpeed={1000}
-      keyBoardControl={true}
-      customTransition="all .5"
-      transitionDuration={500}
-      containerClass="carousel-container"
+      // ssr={true} // means to render carousel on server-side.
+      autoPlay={true}
+      autoPlaySpeed={5000}
+      // keyBoardControl={true}
+      // transitionDuration={500}
       removeArrowOnDeviceType={["tablet", "mobile"]}
       // deviceType={this.props.deviceType}
-      dotListClass="custom-dot-list-style"
-      itemClass="carousel-item-padding-40-px"
+      containerClass={styles.container}
+      itemClass={styles.item}
     >
       <img src={img0} alt="" />
       <img src={img1} alt="" />
