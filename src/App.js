a18c50a94626011d3c1ee8efc189f551e8cc9547
oscarfalero <oscarfalero10@gmail.com>
2021-09-04 22:22:13 -0300

    margin general container into app

--- src/App.js
+++ src/App.js
@@ -6,18 +6,21 @@ import ProductList from "./pages/products/productList/ProductList";
 import Contact from "./pages/contact/Contact";
 import Home from "./pages/home/Home";
 import Post from "./components/post/Post";
+import styles from "./app.module.css";
 
 function App() {
   return (
     <>
       <Router>
         <NavBar />
-        <Switch>
-          <Route path="/" exact component={() => <Home />} />
-          <Route path="/productos" exact component={() => <ProductList />} />
-          <Route path="/recetas" exact component={() => <RecList />} />
-          <Route path="/contacto" exact component={() => <Contact />} />
-        </Switch>
+        <div className={styles.container}>
+          <Switch>
+            <Route path="/" exact component={() => <Home />} />
+            <Route path="/productos" exact component={() => <ProductList />} />
+            <Route path="/recetas" exact component={() => <RecList />} />
+            <Route path="/contacto" exact component={() => <Contact />} />
+          </Switch>
+        </div>
       </Router>
       <Post />
     </>
