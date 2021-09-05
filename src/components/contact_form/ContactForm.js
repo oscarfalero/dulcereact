e522dc678ed6bb25c2cc8995988797d043d035aa
oscarfalero <oscarfalero10@gmail.com>
2021-09-04 21:32:48 -0300

    Sección Contacto Front end

--- src/components/contact_form/ContactForm.js
+++ src/components/contact_form/ContactForm.js
@@ -1,12 +1,49 @@
 import React from "react";
+import styles from "./ContactForm.module.css";
 
 const ContactForm = () => {
   return (
-    <form>
-      <label htmlFor="email">Email Address</label>
-      <input id="email" type="email" name="email" />
-      <textarea id="message" name="message" />
-      <button type="submit">Submit</button>
+    <form action="/" name="contact" method="POST">
+      <div classNameName="form-group">
+        <input
+          type="text"
+          className={styles.form_cont}
+          id="contact-name"
+          name="name"
+          placeholder="Nombre"
+        />
+      </div>
+      <div className="form-group">
+        <input
+          type="email"
+          className={styles.form_cont}
+          id="contact-email"
+          name="email"
+          placeholder="Email"
+        />
+      </div>
+      <div className="form-group">
+        <input
+          type="text"
+          className={styles.form_cont}
+          id="contact-website"
+          name="phone"
+          placeholder="Celular / Teléfono"
+        />
+      </div>
+      <div className="form-group">
+        <textarea
+          className={styles.form_cont}
+          name="message"
+          id="message"
+          cols="30"
+          rows="10"
+          placeholder="Mensaje"
+        ></textarea>
+      </div>
+      <button type="submit" className={styles.btn}>
+        Enviar mensaje
+      </button>
     </form>
   );
 };
--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ContactForm from "../../components/contact_form/ContactForm";
 import TopHeader from "../../components/topheader/TopHeader";
+import styles from "./Contact.module.css";
 
 const Contact = () => {
   return (
     <div>
       <TopHeader />
       <h2>Contacto</h2>
-      <p>Puedes contactarme por mis redes o llenando el formulario</p>
+      <p className={styles.text_box}>
+        Puedes contactarme por mis redes o llenando el formulario
+      </p>
       <ContactForm />
     </div>
   );
