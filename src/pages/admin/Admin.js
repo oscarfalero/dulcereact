914ffbb584a7e2a9f917f805de408fbd6b7fa6b1
oscarfalero <oscarfalero10@gmail.com>
2021-09-22 00:32:23 -0300

    arreglos en card vertical y admin page

--- src/pages/admin/Admin.js
+++ src/pages/admin/Admin.js
@@ -10,13 +10,13 @@ const admin = () => {
     }
 
     return (
-        <div className={styles.admin_panel}>
-            <h2>Panel de Administración</h2>
-            <h3>Lista de Recetas</h3>
-            <Button handleClick={handleClick} btnName="Agregar"/>
-            <h4>Aquí se carga una lista de recetas</h4>
-            <CardVertical/>
-        </div>
+            <div className={styles.admin_panel}>
+                <h2>Panel de Administración</h2>
+                <h3>Lista de Recetas</h3>
+                <Button handleClick={handleClick} btnName="Agregar"/>
+                <h4>Aquí se carga una lista de recetas</h4>
+                <CardVertical title="Pollo con papas" time="?" portions="?" />
+            </div>
     )
 }
 
